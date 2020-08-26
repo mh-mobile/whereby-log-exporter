f7515c89400843b24f7496becf30b8b9ba78ccdd
mh-mobile <mh.mobiler@gmail.com>
2020-08-26 22:21:34 +0900

    Unchecked runtime.lastErrorの対策を追加

--- popup.js
+++ popup.js
@@ -2,22 +2,33 @@ $("#generate").on("click", () => {
   function waitPageLoad(callback) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       const currentTab = tabs.shift();
-
-      if (currentTab.status === "complete") {
-        callback(currentTab);
+      if (currentTab.status === 'complete') {
+        callback(currentTab)
       } else {
         setTimeout(() => {
-          waitPageLoad(callback);
-        }, 50);
+          waitPageLoad(callback)
+        }, 100)
+      }
+    })
+  }
+
+  function ping(currentTab) {
+    chrome.tabs.sendMessage(currentTab.id, null, function (response) {
+      if (chrome.runtime.lastError) {
+        setTimeout(() => {
+          ping(currentTab)
+        }, 1000)
+      } else {
+        if (response) {
+          $("#log").val(response)
+        }
       }
     });
   }
 
   waitPageLoad((currentTab) => {
-    chrome.tabs.sendMessage(currentTab.id, null, function (response) {
-      $("#log").val(response);
-    });
-  });
+    ping(currentTab)
+  })
 });
 
 $("#copy").on("click", () => {
